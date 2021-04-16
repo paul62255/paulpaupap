d80a967ad863b10d22f8d5817f1c5be2f4ae1751
paul62255 <80522339+paul62255@users.noreply.github.com>
2021-04-16 21:10:53 +0900

    2nd python test!
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa so difficult

---
+++ test2.py
@@ -0,0 +1,10 @@
+# test '\n'
+print('String\nNext Line\n')
+# Test '\t':tab:4
+print('Before\tAfter') #"Before After"
+print('tab\t\tTAB') #"tab Tab"
+# Test multi line
+multi='''multi1
+multi2
+multi3 line'''
+print(multi)
